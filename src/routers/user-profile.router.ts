dc9710159b8f0ca335c9cc968e57baa3ba4bd71f
dianck2002 <dianck2002@gmail.com>
2025-08-11 14:56:57 +0700

    rebuild

--- src/routers/user-profile.router.ts
+++ src/routers/user-profile.router.ts
@@ -23,7 +23,7 @@ export default class UserProfileRouter {
   }
 
   private initializeRoutes() {
-    // this.router.post("/get", authenticateToken, this.PropertyController.getDetails);
+    this.router.post("/get1", authenticateToken, this.PropertyController.getDetails);
     // this.router.post("/get", authenticateToken, this.userProfileController.getProfile);
     this.router.patch("/update", this.userProfileController.updateProfile);
     this.router.patch("/update-password", this.userProfileController.updatePassword);
