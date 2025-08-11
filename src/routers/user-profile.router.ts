554f16bd54dcf793509d459de7c461dd9c1c51d1
dianck2002 <dianck2002@gmail.com>
2025-08-11 14:21:03 +0700

    test prop

--- src/routers/user-profile.router.ts
+++ src/routers/user-profile.router.ts
@@ -26,7 +26,8 @@ export default class UserProfileRouter {
     this.router.post("/get", authenticateToken, this.userProfileController.getProfile);
     this.router.patch("/update", this.userProfileController.updateProfile);
     this.router.patch("/update-password", this.userProfileController.updatePassword);
-    this.router.post("/propdetails", authenticateToken, this.PropertyController.getDetails);
+    // this.router.post("/propdetails", authenticateToken, this.PropertyController.getDetails);
+    this.router.post("/propdetails", authenticateToken, this.userProfileController.getProfile);
   }
 
   public getRouter(): Router {
