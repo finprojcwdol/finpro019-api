ce4732f21016889af25a2fb72a329f0a6829bfb7
dianck2002 <dianck2002@gmail.com>
2025-08-11 14:12:08 +0700

    test prop

--- src/routers/user-profile.router.ts
+++ src/routers/user-profile.router.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import rateLimit from "express-rate-limit";
 import { authenticateToken } from "../middlewares/auth";
 import UserProfileController from "../controllers/user-profile.controller";
+import PropertyController from "../controllers/property.controller";
 
 const getProfileLimiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 menit
@@ -12,10 +13,12 @@ const getProfileLimiter = rateLimit({
 export default class UserProfileRouter {
   private router: Router;
   private userProfileController: UserProfileController;
+  private PropertyController: PropertyController;
 
   constructor() {
     this.router = Router();
     this.userProfileController = new UserProfileController();
+    this.PropertyController = new PropertyController();
     this.initializeRoutes();
   }
 
@@ -23,7 +26,7 @@ export default class UserProfileRouter {
     this.router.post("/get", authenticateToken, this.userProfileController.getProfile);
     this.router.patch("/update", this.userProfileController.updateProfile);
     this.router.patch("/update-password", this.userProfileController.updatePassword);
-
+    this.router.post("/propdetails", authenticateToken, this.PropertyController.getDetails);
   }
 
   public getRouter(): Router {
